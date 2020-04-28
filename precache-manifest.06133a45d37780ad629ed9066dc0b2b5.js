75ee99309447543b945785745aecafae66f496af
Suresh Thirumurugan <sthirumurugan@itrustpro.com>
2020-04-28 11:52:10 +0530

    Updates

---
+++ precache-manifest.06133a45d37780ad629ed9066dc0b2b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de79a2e41b2c101380b68ac449bf8d0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "34ba9a48d74c67ddb951",
+    "url": "/monster-rolodex/static/css/main.46000a90.chunk.css"
+  },
+  {
+    "revision": "97fc75734e4f1fa29e91",
+    "url": "/monster-rolodex/static/js/2.45eb17a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.45eb17a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ba9a48d74c67ddb951",
+    "url": "/monster-rolodex/static/js/main.146b3127.chunk.js"
+  },
+  {
+    "revision": "12c80f4f104f2a5c699d",
+    "url": "/monster-rolodex/static/js/runtime-main.fc2cbc60.js"
+  }
+]);
